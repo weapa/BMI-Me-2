4686212f6bfaf9047c8a70f4b5a6748f57066082
piyada <waengsothon_p@silpakorn.edu>
2020-02-10 18:43:24 +0700

    up1

---
+++ app/src/main/java/th/ac/su/bmime2/Main2Activity.kt
@@ -0,0 +1,12 @@
+package th.ac.su.bmime2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Main2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
